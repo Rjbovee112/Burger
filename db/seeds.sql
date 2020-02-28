9ebbffdbad1cccae04bce8fb10e66ff294459ed0
Robert Bovee <Rjbovee112@gmail.com>
2020-02-28 13:15:42 -0700

    added hmtl/css

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,3 +17,18 @@ INSERT INTO
 VALUES
     ("Big Tuna", false);
 
+    INSERT INTO
+    burgers (burger_name, devoured)
+VALUES
+    ("Western Burger", false);
+
+    INSERT INTO
+    burgers (burger_name, devoured)
+VALUES
+    ("Blue Cheese Burger", false);
+
+    INSERT INTO
+    burgers (burger_name, devoured)
+VALUES
+    ("Avacado Burger", false);
+
