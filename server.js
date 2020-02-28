47e20f77a9debd44d76bff93ffe836d14ad8aad4
Robert Bovee <Rjbovee112@gmail.com>
2020-02-28 10:58:43 -0700

    adj server.js code to work

--- server.js
+++ server.js
@@ -1,42 +1,23 @@
 
 var express = require("express");
-var exphbs = require("express-handlebars");
-var orm = require("./config/orm");
-const cTable = require("console.table");
-
-var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-///
-/////
-/////// this is for using handlebars
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-// 
-// //
-//  // //
-// start of ORM code
-var burger = "asdflj";
-orm.selectAll();
-// orm.insertOne("nommy");
-// orm.updateOne("freekingWorked", 2);
 
+var routes = require("./controllers/burgers_controller.js");
 
+app.use("/", routes);
 
-
-
-
-
-// start the server
+var PORT = process.env.PORT || 8000;
 app.listen(PORT, function () {
-    console.log("App now listening at localhost:" + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
-
