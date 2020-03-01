572158d2855055e4f59bfb967a3141fef113e8c1
Robert Bovee <Rjbovee112@gmail.com>
2020-02-29 17:24:49 -0700

    js updates

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm.js");
+
+//cb stands for the callback function. same as .then in sequelize.
+var burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+
+module.exports = burger;
